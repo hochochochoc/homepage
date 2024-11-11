0e7c233c0ac216c2d2080eb0181e28dbd008c719
jannik_riegel <jannik.riegel@gmail.com>
2024-11-11 14:35:33 +0100

    reverted firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCBA80E2oupp1pmSuZCEXN9TfnZ1CQXuts",
+  authDomain: "gymtracker-42628.firebaseapp.com",
+  projectId: "gymtracker-42628",
+  storageBucket: "gymtracker-42628.firebasestorage.app",
+  messagingSenderId: "179865169864",
+  appId: "1:179865169864:web:1c8c1bb159dcc6f5455afd",
+  measurementId: "G-0JP5VNMSVK",
 };
 
 const app = initializeApp(firebaseConfig);
